a61d83837602363db762c793dbb52de0200945d7
ABDALRZAQ345 <jmalj6564@gmail.com>
2025-06-28 10:26:37 +0300

    add docker deployment files

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+
+
+composer install  --optimize-autoloader --no-scripts
+#--no-dev
+php artisan package:discover --ansi
+php artisan key:generate
+
+
+#php artisan migrate:fresh --seed
+#php artisan queue:table
+
+mkdir -p storage/app/public
+php artisan storage:link || true
+
+#php artisan queue:work --tries=3 --timeout=90 &
+exec "$@"
